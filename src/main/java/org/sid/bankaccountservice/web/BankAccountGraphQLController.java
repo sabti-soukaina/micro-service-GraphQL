231cdd370f65e09762a06b1b7ca39eaea2725514
sabti-soukaina <159637020+sabti-soukaina@users.noreply.github.com>
2024-06-01 20:14:25 +0100

    main

---
+++ src/main/java/org/sid/bankaccountservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,31 @@
+package org.sid.bankaccountservice.web;
+
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @QueryMapping
+    public List<BankAccount> accountsList(){
+        return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
+    }
+    @MutationMapping
+    public BankAccount addAccount(@Argument BankAccount bankAccount){
+        return bankAccountRepository.save(bankAccount);
+
+    }
+}
